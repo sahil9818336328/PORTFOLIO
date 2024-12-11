c6b85d1443f34bdd613ed33f9ca7497bd849131e
sahil khatri <sahilkeshav1997@gmail.com>
2024-12-11 14:15:58 +0530

    project addition

--- src/constants.js
+++ src/constants.js
@@ -5,21 +5,29 @@ import imgThree from '../src/assets/ProjectsImages/project-3.svg'
 export const works = [
   {
     id: 1,
-    img: imgThree,
+    img: imgOne,
+    title: 'Talk AI (2024)',
+    description: 'Your Next-Gen AI Companion for Seamless Conversations.',
+    stack: 'NextJS, Open AI, React-Query, Clerk, Tailwind Css & Daisy UI.',
+    documentation:
+      'https://www.notion.so/TALK-AI-NEXTJS-OPEN-AI-CLERK-1599833b83988090abc0ddd6e792b934',
+    url: 'https://talk-ai-nine.vercel.app/',
+  },
+  {
+    id: 2,
+    img: imgTwo,
     title: 'Pure Cart (2024)',
-    description:
-      'A user-friendly e-commerce app crafted with React. Experience lightning-fast performance with react (SPA)  and revolutionize your online shopping experience.',
+    description: 'Revolutionizing Online Shopping with a User-Friendly Cart',
     stack: 'React, React-Query,React-Router 6, Tailwind Css, Daisy UI & Redux',
     documentation:
       'https://www.notion.so/PURE-CART-REACT-13a9833b839880ea891df6310f146651',
     url: 'https://purecart-sk-24-tlw-react-query.netlify.app/',
   },
   {
-    id: 2,
-    img: imgOne,
+    id: 3,
+    img: imgThree,
     title: 'Jobs Tracker (2023)',
-    description:
-      ' A simple job tracking software that allows you to perform CRUD operations with proper authentication and serve side pagination.',
+    description: 'Simplifying Job Search and Application Monitoring.',
     stack: 'MERN',
     documentation:
       'https://www.notion.so/JOB-TRACKER-MERN-STACK-7bbda1d00a1849fbbbe6deaf860262f0?pvs=4',
